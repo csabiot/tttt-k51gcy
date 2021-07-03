b84cef28dd2c77ba05b4235e12d3c6523111a722
csabiot <carlossabiotomas@gmail.com>
2021-07-03 17:16:01 +0200

    2 files modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AsyncExerciseModule } from './async-exercise/async-exercise.module';
 
 @NgModule({
-  imports: [BrowserModule, AsyncExerciseModule],
+  imports: [BrowserModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
