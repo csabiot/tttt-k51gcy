5fe7e9698ec9e9dc1a6ca7827bf3069d78dfa6dd
csabiot <86884962+csabiot@users.noreply.github.com>
2021-07-21 12:41:24 +0200

    Update app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AsyncExerciseComponent } from './async-exercise/components/async-exercise.component';
+import { AsyncExerciseService } from './async-exercise/services/async-exercise.service';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, AsyncExerciseComponent],
+  bootstrap: [AppComponent],
+  providers: [AsyncExerciseService]
 })
 export class AppModule {}
